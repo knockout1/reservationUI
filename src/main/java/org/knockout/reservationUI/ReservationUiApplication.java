d351d6ad352720256717719e31fb8873207743c1
Karol Olek <karol.olek@sabre.com>
2019-07-30 14:36:06 +0200

    intial version, added vaadin

---
+++ src/main/java/org/knockout/reservationUI/ReservationUiApplication.java
@@ -0,0 +1,13 @@
+package org.knockout.reservationUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationUiApplication.class, args);
+	}
+
+}
